e0310c374f7bcce6c1e763e4cf09b5c79493615f
R1999T <raghavtiwari7151@gmail.com>
2023-03-03 10:57:35 +0530

    day3 also included , login page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:catelog_proj/home_page.dart';
+import 'package:catelog_proj/pages/home_page.dart';
+import 'package:catelog_proj/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+
+      //routes: takes map values
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
